e6557533073b291d46dc83188aea0bf25cf19a30
= <mrbartrns@naver.com>
2020-07-24 18:44:28 +0900

    add key eventListener

--- mdn_2d_breakout/2d_breakout.js
+++ mdn_2d_breakout/2d_breakout.js
@@ -1,23 +1,66 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
+let rightPressed = false;
+let leftPressed = false;
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
+let x = width / 2;
+let y = height - 30;
 let dx = 2;
 let dy = -2;
+let radius = 10;
+let color = `rgba(0, 0, 255, 0.25)`;
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (width - paddleWidth) / 2;
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = `rgba(0, 0, 255, 0.25)`;
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
 }
 
+function keyDownHandler(event) {
+    if (event.keyCode === 39) {
+        rightPressed = true;
+    } else if (event.keyCode === 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if (event.keyCode === 39) {
+        rightPressed = false;
+    } else if (event.keyCode === 37) {
+        leftPressed = false;
+    }
+}
+
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //clear previous frame
+    ctx.clearRect(0, 0, width, height);
     drawBall();
     x += dx;
     y += dy;
+    if ((x + radius) >= width || (x - radius) <= 0) {
+        dx = -dx;
+        color = `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.25)`;
+    }
+    if ((y + radius) >= height || (y - radius) <= 0) {
+        dy = -dy;
+        color = `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.25)`;
+    }
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10);
