529f53eb34a56e4d781d13736d6f14e326694fe0
= <mrbartrns@naver.com>
2020-08-21 17:14:09 +0900

    made drag function in button click

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,182 @@
+// strings arr랑 objarr랑 갯수를 맞추기
+const scriptInput = document.querySelector(".script");
+const showing = document.querySelector(".showing");
+const showingP = showing.querySelector("p");
+const modes = document.querySelectorAll(".mode");
+const ENTER = 13;
+const BACKSPACE = 8;
+const SELECTED = "selected";
+const action = document.querySelector(`.${SELECTED}`).dataset.type;
+
+let dialogObjects = [];
+let stringsLength = 0;
+let currentLine = 0;
+// END OF GLOBAL SCOPE'S VARIABLES
+
+function Dialog(action) {
+    this.row = dialogObjects.length + 1;
+    this.action = action;
+    this.script = "";
+}
+
+Dialog.prototype.getRow = function() {
+    return this.row;
+}
+
+Dialog.prototype.getAction = function() {
+    return this.action;
+}
+
+Dialog.prototype.getScript = function() {
+    return this.script;
+}
+
+Dialog.prototype.setRow = function(row) {
+    this.row = row;
+}
+
+Dialog.prototype.setAction = function(action) {
+    this.action = action;
+}
+
+Dialog.prototype.setScript = function(text) {
+    this.script = text;
+}
+// END OF OBJECTS
+
+function getType(row) {
+    // 줄을 바탕으로 버튼에 어떤 action인지 표시한다.
+    const currentAction = dialogObjects[row - 1].getAction();
+    console.log(currentAction);
+    modes[0].classList.remove(SELECTED);
+    modes[1].classList.remove(SELECTED);
+    if (currentAction === "dialog") {
+        modes[0].classList.add(SELECTED);
+    } else {
+        modes[1].classList.add(SELECTED);
+    }
+}
+
+function refreshDialogs(strings) {
+    dialogObjects.forEach((object,index) => {
+        object.setRow(index + 1);
+        object.setScript(strings[index]);
+    })
+}
+
+function getTextArr() {
+    const value = scriptInput.value;
+    const strings = value.split("\n");
+    return strings;
+}
+
 function getRow(textarea) {
     const txts = textarea.value;
-    const strings = txts.split("\n");
+    const txtsArr = txts.split("\n");
     const cursorPoint = textarea.selectionStart;
     let lineStart = 0;
-    for (let i = 0; i < strings.length; i++) {
-        let lineEnd = lineStart + strings[i].length + 1;
+    for (let i = 0; i < txtsArr.length; i++) {
+        let lineEnd = lineStart + txtsArr[i].length + 1;
         if (lineStart <= cursorPoint && cursorPoint < lineEnd) {
-            let lineIndex = i + 1;
+            const lineIndex = i + 1;
             return lineIndex;
         } else {
             lineStart = lineEnd;
         }
     }
 }
+
+function getDraggedRows(textarea, rowStart) {
+    rowStart = getRow(textarea);
+    const txts = textarea.value;
+    const txtsArr = txts.split("\n");
+    const cursorEnd = textarea.selectionEnd;
+    let lineStart = 0;
+    for (let i = 0; i < txtsArr.length; i++) {
+        let lineEnd = lineStart + txtsArr[i].length + 1;
+        if (lineStart <= cursorEnd && cursorEnd < lineEnd) {
+            const rowEnd = i + 1;
+            const draggedLines = rowEnd - rowStart + 1;
+            return draggedLines;
+        } else {
+            lineStart = lineEnd;
+        }
+    }
+}
+
+function handleEnter() {
+    const strings = getTextArr();
+    const action = document.querySelector(`.${SELECTED}`).dataset.type;
+    console.log(strings);
+    const row = getRow(scriptInput);
+    currentLine = row;
+    const dialogObject = new Dialog(action);
+    dialogObjects.splice(row - 1, 0, dialogObject);
+    console.log(dialogObjects);
+    refreshDialogs(strings);
+    stringsLength = strings.length;
+}
+
+function handleBackSpace() {
+    const strings = getTextArr();
+    const row = getRow(scriptInput);
+    currentLine = row;
+    let currentLength = strings.length;
+    let lengths = stringsLength - currentLength;
+    if (lengths > 0) {
+        dialogObjects.splice(row, lengths);
+        stringsLength = currentLength;
+        refreshDialogs(strings);
+        console.log(dialogObjects);
+    }
+}
+
+// todo: modify setUpMode when I select row showing action
+function setUpMode() {
+    const row = getRow(scriptInput);
+    modes[0].classList.remove(SELECTED);
+    modes[1].classList.remove(SELECTED);
+    this.classList.add(SELECTED);
+    const action = document.querySelector(`.${SELECTED}`).dataset.type;
+    // drag 구현하기
+    if (scriptInput.selectionStart !== scriptInput.selectionEnd) {
+        const draggedLines = getDraggedRows(scriptInput, row);
+        for (let i = row - 1; i < row - 1 + draggedLines; i++) {
+            dialogObjects[i].setAction(action);
+        }
+    } else {
+        //드래그 안했을때
+        dialogObjects[row - 1].setAction(action);
+    }
+    console.log(dialogObjects);
+}
+
+function init() {            
+    const dialogObject = new Dialog(action);
+    dialogObjects.push(dialogObject);
+
+    modes.forEach( mode => {
+        mode.addEventListener("click", setUpMode);
+    });
+
+    scriptInput.addEventListener("keyup", function(e) {
+        if (e.keyCode === ENTER) {
+            handleEnter();
+        } else if (e.keyCode === BACKSPACE) {
+            handleBackSpace();
+        }
+    });
+
+    //toDo: scriptInput.addEventListener("click", handleClick);
+    scriptInput.addEventListener("click", function() {
+        const row = getRow(scriptInput);
+        currentLine = row;
+        console.log(row);
+        getType(row);
+        // if (scriptInput.selectionStart !== scriptInput.selectionEnd) {
+        //     console.log(getDraggedRows(scriptInput));
+        // }
+    });
+}
+
+init();
