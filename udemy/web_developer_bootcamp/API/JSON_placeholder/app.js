23607a48adbcb501852568e888b37e5d3353859f
Junhyuck <mrbartrns@naver.com>
2020-09-10 15:49:37 +0900

    search practice

--- udemy/web_developer_bootcamp/API/JSON_placeholder/app.js
+++ udemy/web_developer_bootcamp/API/JSON_placeholder/app.js
@@ -4,8 +4,14 @@ const request = require('request');
 
 app.set('view engine', 'ejs');
 
+app.get("/", (req, res) => {
+    res.render("search");
+});
+
 app.get("/results", (req, res) => {
-    request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", (error, response, body) => {
+    const query = req.query.search;
+    const url = 'http://www.omdbapi.com/?s=' + query;
+    request(url, (error, response, body) => {
         if(!error && response.statusCode === 200) {
             const parsed = JSON.parse(body);
             // res.send(parsed['Search'][0]);
