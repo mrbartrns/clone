11a481b1cd01ecef88d985a806ab8c7b5a4860f4
Junhyuck <mrbartrns@naver.com>
2020-10-09 09:55:00 +0900

    crud

--- node/portal/CRUD/app.js
+++ node/portal/CRUD/app.js
@@ -3,35 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mysql = require('mysql');
-const passport = require('passport');
-const socketio = require('socket.io');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login');
-const boardRouter = require('./routes/board');
-const postRouter = require('./routes/post');
 
 const app = express();
-
-const io = socketio();
-app.io = io;
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.currentUser = req.user;
-  next();
-})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -41,18 +21,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/board', boardRouter);
-app.use('/post', postRouter);
-
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -61,10 +37,5 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
-
-// app.io.on()
-app.io.on('connection', socket => {
-  console.log('connected');
-})
 
 module.exports = app;
